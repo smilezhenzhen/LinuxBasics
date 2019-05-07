04ec4ebe10a29a1e76df522376bec75c6ac269e6
smilezhenzhen <785042858@qq.com>
2019-05-07 17:09:20 +0800

    更新lvs-dr模式下lvs服务器的配置文件

--- 1_linux_work/work2_nginx_load_balance/lvs_conf.sh
+++ 1_linux_work/work2_nginx_load_balance/lvs_conf.sh
@@ -2,25 +2,27 @@
 
 vip='172.29.229.250'
 iface='eth0:0'
-mask='255.255.255.0'
+mask='255.255.255.255'
 port='80'
 nginx1='172.29.229.200'
 nginx2='172.29.229.202'
-scheduler='wrr'
-type='-g'
-drgw='172.29.229.194'
+#drgw='172.29.229.194'
 
+#./etc/rc.d/init.d/functions   #调用init.d脚本的标准库
 case $1 in
 start)
 	echo "start lvs of dr mode"
-	echo "1" > /proc/sys/net/ipv4/ip_forward      #开启路由转发功能
 	ifconfig $iface $vip netmask $mask broadcast $vip up #绑定虚拟ip
-	route add default gw $drgw
+	route add -host $vip dev $iface          #添加路由规则
+	echo "1" > /proc/sys/net/ipv4/ip_forward #开启路由转发功能
+	ipvsadm --clear    #清除原有转发规则
+	#route add default gw $drgw
 	iptables -F
-	ipvsadm -A -t ${vip}:${port} -s $scheduler           #添加虚拟服务记录
-	ipvsadm -a -t ${vip}:${port} -r ${nginx1} $type -w 1 #添加真实服务记录
-	ipvsadm -a -t ${vip}:${port} -r ${nginx2} $type -w 1 #添加真实服务记录
+	ipvsadm -A -t ${vip}:${port} -s rr           #添加虚拟IP规则
+	ipvsadm -a -t ${vip}:${port} -r ${nginx1}:${port} -g  #在虚拟IP中添加服务规则
+	ipvsadm -a -t ${vip}:${port} -r ${nginx2}:${port} -g  #在虚拟IP中添加服务规则
 	ipvsadm --set 30 120 300   #设置tcp tcpfin udp的超时连接值
+	ipvsadm
 	;;
 stop)
 	echo "stop lvs dr"
